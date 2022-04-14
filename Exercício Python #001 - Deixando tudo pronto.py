d6159b553199a48ff872e23c982cbe95196104c6
Shrek185 <shrek18.5@gmail.com>
2022-04-14 20:41:55 -0300

    Initial commit

---
+++ Exercício Python #001 - Deixando tudo pronto.py
@@ -0,0 +1,13 @@
+"""
+Autor : Douglas Nascimento (Shrek18.5)
+
+Obs:    Curso Python - Exercício Python #001 - Deixando tudo pronto - Curso em Video Gustavo Guanabara
+        Video base para o curso de ADS (EAD) Uninter 2022
+
+        link: https://www.youtube.com/watch?v=nIHq1MtJaKs&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=7
+
+"""
+msg = 'Olá Mundo!'
+print(msg)
+sms = 'teste'
+print(sms)
---
+++ Exercício Python #002 - Respondendo ao Usuário.py
@@ -0,0 +1,14 @@
+"""
+Autor : Douglas Nascimento (Shrek18.5)
+
+Obs:    Curso Python - Exercício Python #001 - Deixando tudo pronto - Curso em Video Gustavo Guanabara
+        Video base para o curso de ADS (EAD) Uninter 2022
+
+        https://www.youtube.com/watch?v=FNqdV5Zb_5Q&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=9
+
+        #Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+"""
+nome = input('Digite o seu nome! ')
+
+print('É um prazer te conhecer, {}!'.format(nome))
